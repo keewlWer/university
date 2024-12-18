de012f6650ee6697a52697157ed477c8dc440606
deepwoodself <92952187+keewlWer@users.noreply.github.com>
2024-12-18 07:35:44 +0200

    Create ex3.py

---
+++ stack/ex3.py
@@ -0,0 +1,31 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val: int):
+        self.stack.append(val)
+        if not self.min_stack or val <= self.min_stack[-1]:
+            self.min_stack.append(val)
+
+    def pop(self):
+        val = self.stack.pop()
+        if val == self.min_stack[-1]:
+            self.min_stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
+
+# Тестування:
+minStack = MinStack()
+minStack.push(-2)
+minStack.push(0)
+minStack.push(-3)
+print(minStack.getMin())  # Output: -3
+minStack.pop()
+print(minStack.top())     # Output: 0
+print(minStack.getMin())  # Output: -2
